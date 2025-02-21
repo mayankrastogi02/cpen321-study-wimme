808bf4f1e3bdf1fe9b4113f37d28f647e623f26b
davidcdeng <davidcdeng@gmail.com>
2025-02-21 01:22:27 -0800

    endpoints for sending friend request and adding friend

--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -5,6 +5,18 @@ const controller = new UserController();
 export const UserRoutes = [
     {
         method: "post",
+        route: "/user/friendRequest",
+        action: controller.sendFriendRequest,
+        validation: [],
+    },
+    {
+        method: "post",
+        route: "/user/addFriend",
+        action: controller.addFriend,
+        validation: [],
+    },
+    {
+        method: "post",
         route: "/user",
         action: controller.createUser,
         validation: [],
--- server/schemas/UserSchema.ts
+++ server/schemas/UserSchema.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
+    userName: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     year: { type: Number, min: 1, required: true },
     faculty: { type: String, required: true },
-    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],  
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
   
   const User = mongoose.model("User", UserSchema);
