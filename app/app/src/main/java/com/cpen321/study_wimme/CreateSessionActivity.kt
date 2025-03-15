caab00fcd6be11f868d39b0a5753470a8f2cad13
Yibo Chen <yibochen2019@gmail.com>
2025-03-14 17:19:13 -0700

    Initial Espresso Tests

--- app/app/src/main/java/com/cpen321/study_wimme/CreateSessionActivity.kt
+++ app/app/src/main/java/com/cpen321/study_wimme/CreateSessionActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.button.MaterialButtonToggleGroup
@@ -381,4 +382,11 @@ class CreateSessionActivity : AppCompatActivity() {
             )
         }
     }
+
+    @VisibleForTesting
+    fun setTestDates(startMillis: Long, endMillis: Long) {
+        startDateMillis = startMillis
+        endDateMillis = endMillis
+    }
+
 }
